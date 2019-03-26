01797896ba549303436a8b93e56de3c11058c940
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-03-26 06:13:26 +0000

    test(normalize): add tests for normalize

--- index.test.ts
+++ index.test.ts
@@ -1,3 +1,18 @@
 import test from "ava";
+import {normalize} from "./index";
 
-test("placeholder", t => t.true(true));
+test("normalize", t => {
+    const {toString} = normalize({});
+    t.deepEqual(normalize({}), {units: 0, billionths: 0, toString});
+    t.deepEqual(normalize({units: 1}), {units: 1, billionths: 0, toString});
+    t.deepEqual(normalize({billionths: 1}), {units: 0, billionths: 1, toString});
+    t.deepEqual(normalize({units: 1, billionths: 1}), {units: 1, billionths: 1, toString});
+    t.deepEqual(normalize({units: -1}), {units: -1, billionths: 0, toString});
+    t.deepEqual(normalize({billionths: -1}), {units: 0, billionths: -1, toString});
+    t.deepEqual(normalize({units: 1, billionths: -1}), {units: 0, billionths: 999999999, toString});
+    t.deepEqual(normalize({units: -1, billionths: 1}), {units: 0, billionths: -999999999, toString});
+    t.deepEqual(normalize({units: 1.1}), {units: 1, billionths: 100000000, toString});
+    t.deepEqual(normalize({units: 1.1, billionths: 123456789}), {units: 1, billionths: 223456789, toString});
+    t.deepEqual(normalize({units: -1.1}), {units: -1, billionths: -100000000, toString});
+    t.deepEqual(normalize({units: -2, billionths: 1}), {units: -1, billionths: -999999999, toString});
+});
