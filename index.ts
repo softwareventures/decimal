0fb4e7748ab049204c4b3754ae9c44fc3f9f5192
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-03-26 01:31:13 +0000

    feat: implement normalize function and toString method

--- index.ts
+++ index.ts
@@ -4,3 +4,62 @@ export default interface Decimal {
 
     toString(): string;
 }
+
+function toString(this: Partial<Readonly<Decimal>>): string {
+    const {units, billionths} = normalize(this);
+    if (!isFinite(units) || Math.abs(billionths) < 0.5 || units >= 1e21) {
+        return "" + units;
+    } else if (units < 0 || billionths < 0) {
+        return "-" + (-units) + "."
+            + ("00000000" + Math.round(-billionths))
+                .substr(-9)
+                .replace(/0*$/, "");
+    } else {
+        return "" + units + "."
+            + ("00000000" + Math.round(billionths))
+                .substr(-9)
+                .replace(/0*$/, "");
+    }
+}
+
+export function normalize(value: Partial<Readonly<Decimal>>): Decimal {
+    let units = value.units == null
+        ? 0
+        : Math.floor(value.units);
+
+    let billionths: number;
+
+    if (isFinite(units)) {
+        billionths = value.units == null
+            ? 0
+            : (value.units - units) * 1e9;
+
+        if (value.billionths != null) {
+            billionths += value.billionths;
+        }
+
+        if (isFinite(billionths)) {
+            if (billionths >= 1e9) {
+                units += Math.floor(billionths / 1e9);
+                billionths -= Math.floor(billionths / 1e9) * 1e9;
+            } else if (billionths <= -1e9) {
+                units += Math.ceil(billionths / 1e9);
+                billionths -= Math.ceil(billionths / 1e9) * 1e9;
+            }
+
+            if (units > 0 && billionths < 0) {
+                units -= 1;
+                billionths += 1e9;
+            } else if (units < 0 && billionths > 0) {
+                units += 1;
+                billionths -= 1e9;
+            }
+        } else {
+            units = billionths;
+        }
+    } else {
+        billionths = units;
+    }
+
+    return {units, billionths, toString};
+}
